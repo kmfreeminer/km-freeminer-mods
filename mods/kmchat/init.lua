8e701a6716a0f14604409daea665476f9d9ee8ef
xunto <hunterdelyx1@gmail.com>
2015-10-25 23:14:01 +0300

    Refactoring with new config features, that implemented through callbacks

--- mods/kmchat/init.lua
+++ mods/kmchat/init.lua
@@ -26,17 +26,64 @@
 --  * GM-prefixes
 --  * Dices
 
+-- dices
+function dices_proc(pattern, submes)
+    dice = submes[1]
+    if dice=="4" or dice=="6" or dice=="8" or dice=="10" or dice=="12" or dice=="20" then
+        color = DICE_COLOR
+        dice_result = math.random(dice)
+        pattern_result = string.format(pattern, "%s", dice, "%s")
+        return pattern_result, dice_result
+    else
+        return "%s: %s", nil
+    end
+
+end
+
+-- fudge dices
+fudge_levels = {"-","terrible--","terrible-","terrible", "poor", "mediocre", "fair", "good", "great", "superb", "legendary", "legendary+", "legendary++","like Allah"}
+
+function fudge_proc(pattern, submes)
+    fudge_dice_tmp = submes[1]
+    for key, val in pairs(fudge_levels) do
+        fudge_level = string.match(fudge_dice_tmp, "^("..val..".*)")
+        fudge_level_key = key
+        
+        if fudge_level~=nil then
+            diff = 0
+            signs = ""
+            
+            for i = 1, 4 do
+                rand = math.random(3)
+                if rand == 1 then
+                    diff=diff+1
+                    signs = signs.."+"
+                elseif rand == 2 then
+                    diff=diff-1
+                    signs = signs.."-"
+                else
+                    signs = signs.."="
+                end
+            end
+            
+            pattern_result = string.format(pattern, "%s", signs, fudge_level, "%s")
+
+            fudge_level_key = fudge_level_key+diff
+            
+            if fudge_level_key<1 then
+                fudge_level_key = 1
+            elseif fudge_level_key>#fudge_levels then
+                fudge_level_key = #fudge_levels
+            end
+            
+            dice_result = fudge_levels[fudge_level_key]
+            return pattern_result, dice_result
+        end
+    end
+    return "%s: %s", nil
+end
+
 -- config zone {{{
-formats = {
--- ["MATCH"]        = {"FORMAT"                  RANGE  COLOR     PRIV}, --
-   ["_(.+)"]        = {"%s (OOC): (( %s ))",     18,    "9966AA", nil },
-   ["%(%((.+)%)%)"] = {"%s (OOC): (( %s ))",     18,    "9966AA", nil },
-   ["!(.+)"]       = {"%s (shouts): %s",        68,    "FFFFFF", nil },
-   ["=(.+)"]        = {"%s (whispers): %s",      3,     "E0EEE0", nil },
-   ["*(.+)"]       = {"* %s %s",                18,    "FFFF00", nil },
-   ["#(.+)"]       = {"*** %s: %s ***",         18,    "FFFF00", "gm"},
-   ["?(.+)"]       = {"%s (OOC): %s ***",       31000, "20EEDD", nil },
-}
 DEFAULT_FORMAT     = "%s: %s" 
 DEFAULT_RANGE      = 18
 DEFAULT_COLOR      = "EEF3EE"
@@ -44,7 +91,21 @@ DICE_COLOR         = "FFFF00"
 GMSPY_COLOR        = "666666"
 GM_PREFIX          = "[GM] "
 
-fudge_levels = {"-","terrible--","terrible-","terrible", "poor", "mediocre", "fair", "good", "great", "superb", "legendary", "legendary+", "legendary++","like Allah"}
+-- formats
+formats = {
+-- ["MATCH"]        = {"FORMAT"                                                  RANGE  COLOR       PRIV  PRIV_LIST   PROC_FUNCTION}, --
+   ["^_(.+)"]        = {"%s (OOC): (( %s ))",                                              18,    "9966AA",   nil,      nil,      nil        },
+   ["^%(%((.+)%)%)"] = {"%s (OOC): (( %s ))",                                              18,    "9966AA",   nil,      nil,      nil        },
+   ["^!(.+)"]        = {"%s (shouts): %s",                                                 68,    "FFFFFF",   nil,      nil,      nil        },
+   ["^=(.+)"]        = {"%s (whispers): %s",                                               3,     "E0EEE0",   nil,      nil,      nil        },
+   ["^*(.+)"]        = {"* %s %s",                                                         18,    "FFFF00",   nil,      nil,      nil        },
+   ["^#(.+)"]        = {"*** %s: %s ***",                                                  18,    "FFFF00",   "gm",     nil,      nil        },
+   ["^?(.+)"]        = {"%s (OOC): %s ***",                                                31000, "20EEDD",   nil,      nil,      nil        },
+   ["^d(%d+).*"]     = {"*** %s rolls d%s and the result is %s ***",                       18,    DICE_COLOR, nil,      nil,      dices_proc },
+   ["^sd(%d+).*"]    = {"*** %s rolls d%s silently and the result is %s ***",              3,     DICE_COLOR, nil,      nil,      dices_proc },
+   ["^4dF (.*)$"]    = {"*** %s rolls 4df (%s) from %s and the result is %s ***",          18,    DICE_COLOR, nil,      nil,      fudge_proc },
+   ["^s4dF (.*)$"]    = {"*** %s rolls 4df (%s) from %s silently and the result is %s ***", 18,   DICE_COLOR, nil,      nil,      fudge_proc },
+}
 
 -- config zone }}}
 minetest.register_privilege("gm", "Gives accses to reading all messages in the chat")
@@ -55,14 +116,21 @@ minetest.register_on_chat_message(function(name, message)
     color = DEFAULT_COLOR
     pl = minetest.get_player_by_name(name)
     pls = minetest.get_connected_players()
+    
     -- formats (see config zone)
     for m, f in pairs(formats) do
-        submes = string.match(message, m)
-        if submes then
+        submes = {string.match(message, m)}
+        print(submes[1])
+        if submes[1] then
             if not f[4] then  -- if PRIV==nil
                 fmt = f[1]
                 range = f[2]
                 color = f[3]
+                if f[6]~=nil then
+                    fmt, submes = f[6](fmt, submes)
+                else
+                    submes = submes[1]
+                end
                 break
             elseif minetest.check_player_privs(name, {[f[4]]=true}) then
                 fmt = f[1]
@@ -71,59 +139,12 @@ minetest.register_on_chat_message(function(name, message)
                 break
             end
         end
-    end
-
-    -- dices
-    dice = string.match(message, "^d(%d+).*")
-    if dice=="4" or dice=="6" or dice=="8" or dice=="10" or dice=="12" or dice=="20" then
-        fmt = "*** %s rolls d"..dice.." and the result is %s ***"
-        color = DICE_COLOR
-        submes = math.random(dice)
-    end
-    
-    --Temporary solution for 4dF
-    fudge_dice_tmp = string.match(message, "^4dF (.*)$")
-    if fudge_dice_tmp~=nil then
-        for key, val in pairs(fudge_levels) do
-            fudge_level = string.match(fudge_dice_tmp, "^("..val..".*)")
-            fudge_level_key = key
-            
-            if fudge_level~=nil then
-                diff = 0
-                signs = ""
-                
-                for i = 1, 4 do
-                    rand = math.random(3)
-                    if rand == 1 then
-                        diff=diff+1
-                        signs = signs.."+"
-                    elseif rand == 2 then
-                        diff=diff-1
-                        signs = signs.."-"
-                    else
-                        signs = signs.."="
-                    end
-                end
-                
-                fmt = "*** %s rolls 4df ("..signs..") from "..fudge_level.." and the result is %s ***"
-                color = DICE_COLOR
-                
-                fudge_level_key = fudge_level_key+diff
-                
-                if fudge_level_key<1 then
-                    fudge_level_key = 1
-                elseif fudge_level_key>#fudge_levels then
-                    fudge_level_key = #fudge_levels
-                end
-                
-                submes = fudge_levels[fudge_level_key]
-                break
-            end
-        end
+        submes = submes[1]
     end
     
     if not submes then
         submes = message
+        color = DEFAULT_COLOR
     end
 
     -- GM's prefix
