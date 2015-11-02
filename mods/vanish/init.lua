db8d68b420aead6b6da08805ff5e7aa3931a845e
None <sullome@yandex.com>
2015-11-02 23:20:22 +0300

    Update vanish. Fix nickname hiding.

--- mods/vanish/init.lua
+++ mods/vanish/init.lua
@@ -1,4 +1,5 @@
-vanished_players = {}
+vanish = {}
+vanish.is_vanished = {}
 
 minetest.register_privilege("vanish", "Allow to use /vanish command")
 
@@ -7,17 +8,28 @@ minetest.register_chatcommand("vanish", {
     description = "Make user invisible",
     privs = {vanish = true},
     func = function(name, param)
-        local prop
-        vanished_players[name] = not vanished_players[name]
+        local player = minetest.get_player_by_name(name)
+
+        -- Toggle vanish
+        vanish.is_vanished[name] = not vanish.is_vanished[name]
         
         if vanished_players[name] then
-            prop = {visual_size = {x=0, y=0}, collisionbox = {0,0,0,0,0,0}}
-        else
-            -- default player size
-            prop = {visual_size = {x=1, y=1},
-                    collisionbox = {-0.35, -1, -0.35, 0.35, 1, 0.35}}
+            -- Hide model
+            player:set_properties({
+                visual_size = {x = 0,y = 0},
+                collisionbox = {0, 0, 0, 0, 0, 0,},
+            })
+            -- Hide nickname
+            player:set_nametag_attributes({color = 0x00000000})
+        else 
+            -- Show model
+            player:set_properties({
+                -- default player size
+                visual_size = {x = 1,y = 1},
+                collisionbox = {-0.35, -1, -0.35, 0.35, 1, 0.35},
+            })
+            -- Show nickname
+            player:set_nametag_attributes({color = 0xFFFFFFFF})
         end
-
-        minetest.get_player_by_name(name):set_properties(prop)
     end
 })
