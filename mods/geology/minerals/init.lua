fcf343540bfcf0ed31d944020727d7693bed0826
Sullome <sullome@yandex.com>
2015-11-13 11:59:05 +0300

    Rewrite ores. Add granite. Remove marble and limestone items.

--- mods/geology/minerals/init.lua
+++ mods/geology/minerals/init.lua
@@ -5,31 +5,26 @@ minerals.list = {
     bituminous_coal = "Каменный уголь",
     charcoal        = "Древесный уголь",
     ---------------------------------------
-    iron_ore        = "Железная руда",
     cassiterite     = "Касситерит",
+    cinnabar        = "Киноварь",
     galena          = "Галенит",
+    iron_ore        = "Железная руда",
     native_copper   = "Самородная медь",
     native_gold     = "Самородное золото",
     native_silver   = "Самородное серебро",
-    cinnabar        = "Киноварь",
     saltpeter       = "Селитра",
     sulfur          = "Сера",
     ---------------------------------------
     lazurite        = "Лазурит",
     malachite       = "Малахит",
-    marble          = "Мрамор",
-    limestone       = "Известняк",
 }
 
 for mineral, mineral_desc in pairs(minerals.list) do
-	minetest.register_craftitem("minerals:"..mineral, {
-		description = mineral_desc,
-		inventory_image = "minerals_"..mineral..".png",
-	})
+    minetest.register_craftitem("minerals:"..mineral, {
+        description = mineral_desc,
+        inventory_image = "minerals_"..mineral..".png",
+    })
 end
-
-minetest.override_item("minerals:marble", { groups = {flux = 1} })
-minetest.override_item("minerals:limestone", { groups = {flux = 1} })
 
 --{{{ Fuel registration
 minetest.register_craft({
