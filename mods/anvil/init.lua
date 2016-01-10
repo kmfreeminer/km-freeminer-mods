dda85948cc50e6bca3b134144d452186ddd3fe0e
None <sullome@yandex.com>
2016-01-10 13:28:26 +0300

    Modify anvil mod, using crafter mod now.

--- mods/anvil/init.lua
+++ mods/anvil/init.lua
@@ -11,7 +11,8 @@ anvil.formspec =
     "list[current_name;hammer;1,1;1,1;]" ..
     "list[current_name;src;2.4,0;3,3;]" ..
     "image[5.7,1;1,1;gui_furnace_arrow_bg.png^[transformR270]" ..
-    "list[current_name;dst;7,1;1,1;]" ..
+    "list[current_name;dst;7,0;1,1;3]" ..
+    "list[current_name;dst;7,1;1,2;]" ..
     inventory.main(0, 3.2) ..
     "listring[current_name;dst]" ..
     "listring[current_player;main]" ..
@@ -23,7 +24,7 @@ anvil.formspec =
 --{{{ Craft protection
 minetest.register_on_craft(function(itemstack, player, old_craft_grid, craft_inv)
     if metals.contains_metals(old_craft_grid)
-    and itemstack:get_name():sub(1, itemstack:get_name():find(":") - 1) ~= "anvil"
+    and itemstack:get_modname() ~= "anvil"
     then
         minetest.chat_send_player(player:get_player_name(),
             "Этот предмет нельзя создать голыми руками."
@@ -38,15 +39,23 @@ end)
 local function is_hammer(itemstack)
     local name = itemstack:get_name()
     local tool = minetest.registered_tools[name]
-    if tool ~= nil and tool.tool_capabilities.groupcaps.anvil ~= nil
-    then return true
-    else return false
+
+    if tool ~= nil
+    and tool.tool_capabilities.groupcaps.anvil ~= nil
+    and tool.tool_capabilities.groupcaps.anvil > 0
+    then
+        return true
+    else
+        return false
     end
 end
 
 local function get_uses(itemstack)
     local name = itemstack:get_name()
-    return minetest.registered_tools[name].tool_capabilities.groupcaps.anvil.uses
+    local tool = minetest.registered_tools[name]
+    if tool then
+        return tool_capabilities.groupcaps.anvil.uses
+    end
 end
 
 anvil.craft_predict = function(pos, player)
@@ -54,27 +63,29 @@ anvil.craft_predict = function(pos, player)
     local inv = meta:get_inventory()
     local craftlist = inv:get_list("src")
 
-    local output, decr_input = minetest.get_craft_result({
-        method = "normal",
+    -- Get recipes for anvil
+    local output = crafter.get_craft_result({
+        method = "anvil",
         width = inv:get_width("src") or 3,
         items = craftlist
     })
-
-    if output.item:get_name() ~= "" and output.time == 0 then
-        if metals.contains_metals(craftlist)
-        and output.item:get_name():sub(1, output.item:get_name():find(":") - 1) ~= "anvil"
-        then
-            local hammer = inv:get_stack("hammer", 1)
-            if hammer:get_name() ~= "" then
-                inv:set_stack("dst", 1, output.item)
-            else
-                inv:set_stack("dst", 1, nil)
-            end
-        else
-            inv:set_stack("dst", 1, output.item)
+    print(dump(output))
+    if #output > 3 then
+        minetest.log("error",
+            "Too many craft recipes (" .. #output .. ") for craftlist " ..
+            craftlist
+        )
+        for i = 4, #output do
+            output[i] = nil
         end
+    end
+
+    local hammer = inv:get_stack("hammer", 1)
+    if hammer:get_name() ~= "" then
+        -- If hammer is present
+        inv:set_list("dst", output)
     else
-        inv:set_stack("dst", 1, nil)
+        inv:set_list("dst", {})
     end
 end
 
@@ -83,35 +94,29 @@ anvil.craft = function (pos, player)
     local inv = meta:get_inventory()
     local craftlist = inv:get_list("src")
 
-    local output, decr_input = minetest.get_craft_result({
-        method = "normal",
-        width = inv:get_width("src") or 3,
-        items = craftlist
-    })
+    local hammer = inv:get_stack("hammer", 1)
+    if hammer:get_name() ~= "" then
+        local hammer_level = minetest.get_item_group(hammer, "level")
+        local output_level = minetest.get_item_group(output.item, "level")
+        local uses = get_uses(hammer)
 
-    if metals.contains_metals(craftlist) then
-        local hammer = inv:get_stack("hammer", 1)
-        if hammer:get_name() ~= "" then
-            local hammer_level = minetest.get_item_group(hammer, "level")
-            local output_level = minetest.get_item_group(output.item, "level")
-            local uses = get_uses(hammer)
+        local leveldiff = hammer_level - output_level
 
-            local leveldiff = hammer_level - output_level
+        local multiplier = anvil.multiplier_default
+        if leveldiff < 0 then multiplier = anvil.multiplier_neg end
 
-            local multiplier = anvil.multiplier_default
-            if leveldiff < 0 then multiplier = anvil.multiplier_neg end
+        -- uses | leveldiff | actual uses
+        -- 10   |  0        | 10
+        -- 10   |  1        | 10*3
+        -- 10   | -1        | 10/2
+        hammer:add_wear(65535/(uses * multiplier^leveldiff))
+        inv:set_stack("hammer", 1, hammer)
+    end
 
-            -- uses | leveldiff | actual uses
-            -- 10   |  0        | 10
-            -- 10   |  1        | 10*3
-            -- 10   | -1        | 10/2
-            hammer:add_wear(65535/(uses * multiplier^leveldiff))
-            inv:set_stack("hammer", 1, hammer)
-        end
-        inv:set_list("src", decr_input.items)
-
-    else
-        inv:set_list("src", decr_input.items)
+    -- decrease input
+    for k, itemstack in ipairs(craftlist) do
+        itemstack:take_item()
+        inv:set_stack("src", k, itemstack)
     end
 
     minetest.log("action",
@@ -122,13 +127,13 @@ anvil.craft = function (pos, player)
 end
 
 anvil.register = function (material, anvil_def)
-    local material_name = material:sub(material:find(":") + 1)
+    local material_name = default.strip_modname(material)
     local groups = anvil_def.groups
         or {oddly_breakable_by_hand = 2, falling_node = 1, dig_immediate = 1}
     groups.level = minetest.get_item_group(material, "level")
 
     local material_tile = material_name
-    if material:sub(1, material:find(":") - 1) == "metals" then
+    if default.get_modname(material) == "metals" then
         material_tile = material_tile:sub(1, material_name:find("_unshaped") - 1)
     end
 
@@ -169,7 +174,7 @@ anvil.register = function (material, anvil_def)
             local inv = meta:get_inventory()
             inv:set_size("src", 9)
             inv:set_width("src", 3)
-            inv:set_size("dst", 1)
+            inv:set_size("dst", 3)
             inv:set_size("hammer", 1)
             meta:set_string("formspec", anvil.formspec)
         end,
@@ -200,8 +205,11 @@ anvil.register = function (material, anvil_def)
             end,
         allow_metadata_inventory_move = 
             function (pos, from_list, from_index, to_list, to_index, count, player)
-                if to_list == "dst" or from_list == "dst" then
+                if to_list == "dst" then
                     return 0
+                elseif from_list == "dst" then
+                    anvil.craft(pos, player)
+                    return stack:get_count()
                 elseif to_list == "hammer" then
                     if is_hammer(stack)
                     then return stack:get_count()
@@ -238,6 +246,7 @@ anvil.register = function (material, anvil_def)
             { material, material, material },
         }
     })
+    minetest.log("action", "Registered: anvil:" .. material_name)
 end
 --}}}
 
