90381dd1900d5f46703ef85d673758b75b161371
Xunto <hunterdelyx1@gmail.com>
2016-05-28 17:27:45 +0300

    bugfix

--- mods/charlist/init.lua
+++ mods/charlist/init.lua
@@ -9,9 +9,61 @@ charlist.data_folder = minetest.get_modpath("charlist").."/data/"
 charlist.data = {}
 
 local httpenv = core.request_http_api()
+if not httpenv then
+    minetest.log("error", "Unable to get HTTPApiTable, maybe you should add secure.trusted_mods = roleplay or secure.http_mods = roleplay to your .conf file.")
+end
 
-dofile(minetest.get_modpath("charlist").."/data.lua")
-dofile(minetest.get_modpath("charlist").."/api.lua")
+if require("lfs") then
+    lfs.mkdir(charlist.data_folder)
+else
+    minetest.log("error", "Unable to get lfs, install lfs for lua5.1 or create " .. charlist.data_folder .. " folder yourtself.")
+end
+
+-- Player data save and load functions
+-- {{
+-- Data save functions
+local function file_save(username, data)
+    local storage = io.open(charlist.data_folder .. username, "w")
+    storage:write(minetest.serialize(data))
+    storage:close()
+end
+
+-- Data load functions
+local function file_load(username)
+    local storage = io.open(charlist.data_folder ..username, "r")
+    
+    if not storage then
+        return nil
+    end
+    
+    local result = minetest.deserialize(storage:read("*all"))
+    if type(result) ~= "table" then
+        return nil
+    end
+    
+    storage:close()
+    
+    charlist.data[username] = result
+end
+
+-- Server load function
+local function server_load(httpenv, username)
+    if httpenv then
+        local url = string.gsub(charlist.user_get_url, "{{username}}", username)
+        httpenv.fetch({ url = url, timeout = 2 }, 
+            function(result)
+                if result.succeeded then
+                    local fetched_data = minetest.parse_json(result.data)
+                    if type(fetched_data) == "table" then
+                        charlist.data[username]["name"]   = fetched_data["name"]
+                        charlist.data[username]["quenta"] = fetched_data["quenta"]
+                        charlist.data[username]["skills"] = fetched_data["skills"]
+                    end
+                end
+            end)
+    end
+end
+-- }}
 
 -- Quenta
 -- {{
@@ -82,13 +134,21 @@ end
 
 -- Load data from server on join
 minetest.register_on_joinplayer(function(player)
-    local username = player:get_player_name()    
-    charlist.load_data(httpenv, username)
+    local username = player:get_player_name()
+    
+    charlist.data[username] = {}
+    charlist.data[username].skills = {}
+    charlist.data[username].wounds = {}
+
+    file_load(username)
+    charlist.data[username].color = "AAAAAA"
+    server_load(httpenv, username)
 end)
 
 -- Clear user on leave
 minetest.register_on_leaveplayer(function(player)
     local username = player:get_player_name()
-    charlist.save_data(username, charlist.data[username])
+    
+    file_save(username, data)
+    charlist.data[username] = nil
 end)
-
