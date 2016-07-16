cdab29c1ad4358695704683e91b1f1648f893a99
Sullome <sullome@yandex.com>
2016-07-16 18:34:40 +0300

    Inventory: tooltips colors.

--- mods/gui/init.lua
+++ mods/gui/init.lua
@@ -1,7 +1,7 @@
 gui = {}
 gui.bg = "bgcolor[#080808BB;true]"
 gui.bg_img = "background[5,5;1,1;gui_formbg.png;true]"
-gui.slots = "listcolors[#00000069;#5A5A5A;#141318;#30434C;#FFF]"
+gui.slots = "listcolors[#00000069;#5A5A5A;#141318;#F8F2E7;#000]"
 function gui.get_hotbar_bg(x,y)
 	local out = ""
 	for i=0,7,1 do
