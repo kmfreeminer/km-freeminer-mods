0e043834870c6e6b8423468de6e9c8d2d400beaa
Xunto <hunterdelyx1@gmail.com>
2016-08-09 03:53:30 +0300

    Reload is now used for loading and reloading any user data

--- mods/auth/init.lua
+++ mods/auth/init.lua
@@ -1,8 +1,43 @@
-auth = {}
+local auth = {}
 auth.user_data = {}
 
 auth.handler = {
 	get_auth = function(username)
+        assert(type(username) == "string")
+        return auth.user_data[username]
+	end,
+    clear_auth = function(username)
+        auth.user_data[username] = nil
+    end,
+    create_auth = function(username, password)
+        assert(type(username) == "string")
+        return auth.handler.reload(username)
+	end,
+	set_privileges = function(username, privileges)
+        assert(type(username) == "string")
+		assert(type(privileges) == "table")
+        
+        -- TODO: database.execute, set privileges
+        
+        assert(auth.handler.reload(username))
+	end,
+	set_password = function(username, password_hash)
+        assert(type(username) == "string")
+		assert(type(password_hash) == "string")
+        
+        database.execute([[
+            UPDATE `users`
+            SET    `password_hash` = '%s' 
+            WHERE  `users`.`username` = '%s'; 
+        ]], password_hash, username)
+        
+        assert(auth.handler.reload(username))
+	end,
+    reload = function(username)
+        assert(type(username) == "string")
+        
+        auth.handler.clear_auth(username)
+        
         local user = database.execute([[
             SELECT `user`.`password_hash` FROM `users` as `user`
             WHERE `user`.`username`='%s'
@@ -10,41 +45,34 @@ auth.handler = {
         ]], username)()
         
         if not user then
-            return nil
+            return false
         end
         
-        return {
-			password = user.password_hash,
-			privileges = {}
-        }
-	end,
-    
-    create_auth = function(name, password)
-        return false
-	end,
-
-	set_privileges = function(name, privileges)
+        local active_character = charlist.get_active_character(username)
         
-	end,
-
-	set_password = function(username, password_hash)
-        database.execute([[
-            UPDATE `users`
-            SET    `password_hash` = '%s' 
-            WHERE  `users`.`username` = '%s'; 
-        ]], password_hash, username)
-		return true
-	end,
-
-    reload = function()
-    
+        if not active_character then
+            return false
+        end
+        
+        auth.user_data[username] = {}
+        auth.user_data[username].password = user.password_hash
+        auth.user_data[username].privileges = {}
+        
+        core.notify_authentication_modified(username)
+        return true
     end
 }
 
 core.register_authentication_handler(auth.handler)
 
+core.register_on_leaveplayer(function(player)
+    local username = player:get_player_name()
+    auth.handler.clear_auth(username)
+end)
+
 minetest.register_on_joinplayer(function(player)
     local username = player:get_player_name()
+    
     local active_character = charlist.get_active_character(username)
 
     if active_character.class_id == -10 or active_character.class == "ГМ" then
